b3bdda8ee9d0c33db5bcfd66e47b84fc6bb295d0
ednukros <isanmenarguez@gmail.com>
2023-01-31 17:58:35 +0100

    Learning to create components from terminal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SlidebarComponent } from './slidebar/slidebar.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SlidebarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
